1a08bd77ff79f53cf88277524cd8efd553c2a1e7
Chy Meng <chy@refermybuddy.com>
2019-08-30 00:03:38 +1000

    Add Heading component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 // Components
 import Header from './components/Header'
+import Heading from './components/Heading'
+// Others
 import './App.scss'
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <Heading title='Customise your plan' />
     </div>
   )
 }
